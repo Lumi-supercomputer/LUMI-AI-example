7a6d4b8f3b59a499e98bb71d1115d86a425e0f6a
gregordecristoforo <gregor.decristoforo@gmail.com>
2024-10-08 14:30:17 +0300

    use singularity-userfilesystems singularity-CPEbits

--- run_ddp.sh
+++ run_ddp.sh
@@ -9,6 +9,10 @@
 
 export OMP_NUM_THREADS=7
 
-CONTAINER=/appl/local/containers/sif-images/lumi-pytorch-rocm-6.2.0-python-3.12-pytorch-20240801-vllm-baaedfd.sif
+# shortcut for getting the binds right
+module use /appl/local/training/modules/AI-20240529
+module load singularity-userfilesystems singularity-CPEbits
 
-srun singularity exec --bind /pfs,/scratch,/projappl,/project,/flash,/appl $CONTAINER bash -c '$WITH_CONDA && source myenv_post_upgrade/bin/activate && python -m torch.distributed.run --standalone --nnodes=1 --nproc_per_node=8 ddp_visualtransformer.py'
+CONTAINER=/appl/local/containers/sif-images/lumi-pytorch-rocm-6.2.1-python-3.12-pytorch-20240918-vllm-4075b35.sif
+
+srun singularity exec $CONTAINER bash -c '$WITH_CONDA && source myenv_post_upgrade/bin/activate && python -m torch.distributed.run --standalone --nnodes=1 --nproc_per_node=8 ddp_visualtransformer.py'
