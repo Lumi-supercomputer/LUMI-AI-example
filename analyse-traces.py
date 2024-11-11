2bcbb4a5f736b13bcdb69f49371ce261f16470a3
joasode <joaso@dtu.dk>
2024-11-11 10:18:23 +0200

    Add trace analysis script

---
+++ analyse-traces.py
@@ -0,0 +1,17 @@
+import json
+from glob import glob
+
+
+def getTime(ops, data):
+    # Calculate the total duration of the operation 'ops' from data, arbitrary units
+    return sum([i['dur'] for i in data['traceEvents'] if i['name'] == ops]) / 1000000
+
+
+# It helps to visually analyse the trace on ui.perfetto.dev to identify key operations
+ops = ['hipMemcpyWithStream', 'enumerate(DataLoader)#_MultiProcessingDataLoaderIter.__next__', 'aten::copy_']
+for trace in glob('*-trace.json'):
+    with open(trace, 'r') as fd:
+        data = json.load(fd)
+    print('Analyzing', trace)
+    for op in ops:
+        print('Time of', op, ': ', getTime(op, data))
