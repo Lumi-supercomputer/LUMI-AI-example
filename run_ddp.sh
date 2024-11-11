40e760f6e2d0b2424ab28875d88b020f173115cb
gregordecristoforo <gregor.decristoforo@gmail.com>
2024-11-11 16:54:26 +0200

    Use correct python script for DDP example

--- run_ddp.sh
+++ run_ddp.sh
@@ -7,11 +7,10 @@
 #SBATCH --mem=480G
 #SBATCH --time=1:00:00
 
-
 # shortcut for getting the binds right
 module use /appl/local/training/modules/AI-20240529
 module load singularity-userfilesystems singularity-CPEbits
 
 CONTAINER=/appl/local/containers/sif-images/lumi-pytorch-rocm-6.2.1-python-3.12-pytorch-20240918-vllm-4075b35.sif
 
-srun singularity exec $CONTAINER bash -c '$WITH_CONDA && source myenv_post_upgrade/bin/activate && python -m torch.distributed.run --standalone --nnodes=1 --nproc_per_node=8 ddp_visualtransformer_only_training_dataset.py'
+srun singularity exec $CONTAINER bash -c '$WITH_CONDA && source myenv_post_upgrade/bin/activate && python -m torch.distributed.run --standalone --nnodes=1 --nproc_per_node=8 ddp_visualtransformer.py'
