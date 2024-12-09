3810459e593480973fd72427b888d2b9ff8ff554
joasode <joaso@dtu.dk>
2024-12-09 11:57:40 +0200

    Update run-script

--- run_post_upgrade.sh
+++ run_post_upgrade.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+#SBATCH --job-name=noprof-lmdb
+#SBATCH --output=noprof-lmdb-%j.out
 #SBATCH --account=project_462000002
-#SBATCH --partition=dev-g
-#SBATCH --ntasks=1
+#SBATCH --partition=standard-g
+#SBATCH --nodes=1
 #SBATCH --gpus-per-node=1
 #SBATCH --cpus-per-task=7
-#SBATCH --mem-per-gpu=60G
-#SBATCH --time=1:00:00
+#SBATCH --mem=60G
+#SBATCH --time=24:00:00
 
 # shortcut for getting the binds right
 module use /appl/local/training/modules/AI-20240529
@@ -17,7 +19,13 @@ export MPICH_MPIIO_STATS=1
 export MPICH_MEMORY_REPORT=1
 
 srun singularity exec $CONTAINER bash -c '$WITH_CONDA && source venv-extension/bin/activate && python scripts/lmdb/visualtransformer-lmdb.py'
+
+SQUASH=/scratch/project_462000002/joachimsode/file-format-ai-benchmark/imagenet-object-localization-challenge.squashfs
+IMAGES=/Data/CLS-LOC/train/
+srun singularity exec -B $SQUASH:/train_images:image-src=$IMAGES $CONTAINER bash -c '$WITH_CONDA && source venv-extension/bin/activate && python scripts/squashfs/visualtransformer-squashfs.py'
+
+### No copy
+# srun singularity exec $CONTAINER bash -c '$WITH_CONDA && source venv-extension/bin/activate && python scripts/tar/visualtransformer-tar.py'
 # srun singularity exec $CONTAINER bash -c '$WITH_CONDA && source venv-extension/bin/activate && python scripts/hdf5/visualtransformer-hdf5.py'
 # srun singularity exec $CONTAINER bash -c '$WITH_CONDA && source venv-extension/bin/activate && python scripts/tfrecords/visualtransformer-tfrecords.py'
-srun singularity exec -B data-formats/squashfs/train.squashfs:/train_images:image-src=/,data-formats/squashfs/val.squashfs:/val_images:image-src=/ $CONTAINER bash -c '$WITH_CONDA && source venv-extension/bin/activate && python scripts/squashfs/visualtransformer-squashfs.py'
-# srun singularity exec $CONTAINER bash -c '$WITH_CONDA && source venv-extension/bin/activate && python scripts/tar/visualtransformer-tar.py'
+# srun singularity exec -B data-formats/squashfs/train.squashfs:/train_images:image-src=/ $CONTAINER bash -c '$WITH_CONDA && source venv-extension/bin/activate && python scripts/squashfs/visualtransformer-squashfs.py'
