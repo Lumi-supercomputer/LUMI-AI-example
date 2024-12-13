c2be1efa8a8473da37bd7439263d1bc656aa8070
joasode <joaso@dtu.dk>
2024-12-13 16:20:26 +0200

    Clean up a bit

--- Makefile
+++
@@ -1,9 +0,0 @@
-
-build:
-	./install_venv.sh
-
-download:
-	./get_data.sh
-
-convert:
-	sbatch convert.sh && sbatch scripts/squashfs/convert_to_squashfs.sh
--- convert.sh
+++ convert.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --account=project_462000002
 #SBATCH --partition=small
-#SBATCH --ntasks=3
+#SBATCH --ntasks=2
 #SBATCH --cpus-per-task=16
 #SBATCH --mem-per-cpu=1750
 #SBATCH --time=1:00:00
@@ -13,8 +13,6 @@ CONTAINER=/appl/local/containers/sif-images/lumi-pytorch-rocm-6.2.1-python-3.12-
 
 mkdir -p data-formats/hdf5/
 mkdir -p data-formats/lmdb/
-mkdir -p data-formats/tfrecords/
 
-srun singularity exec $CONTAINER bash -c '$WITH_CONDA && source venv-extension/bin/activate && python scripts/hdf5/convert_to_hdf5.py'
+srun singularity exec $CONTAINER bash -c '$WITH_CONDA && source venv-extension/bin/activate && python scripts/hdf5/convert_to_hdf5.py' &
 srun singularity exec $CONTAINER bash -c '$WITH_CONDA && source venv-extension/bin/activate && python scripts/lmdb/convert_to_lmdb.py'
-srun singularity exec $CONTAINER bash -c '$WITH_CONDA && source venv-extension/bin/activate && python scripts/tfrecords/convert_to_tfrecords.py && python3 -m tfrecord.tools.tfrecord2idx data-formats/tfrecords/train && python3 -m tfrecord.tools.tfrecord2idx data-formats/tfrecords/val'
