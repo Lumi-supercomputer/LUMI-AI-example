642adeb52857e224186aae7b816117385ed6c78d
Jørn D <jdi010@uit.no>
2025-01-22 15:57:07 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --account=project_462000002
-#SBATCH --partition=dev-g
-#SBATCH --gpus-per-node=1
+#SBATCH --account=project_XXX  # project account to bill 
+#SBATCH --partition=dev-g      # other options are small-g and standard-g
+#SBATCH --gpus-per-node=1      # Number of GPUs per node (max of 8)
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=7
-#SBATCH --mem-per-gpu=60G
+#SBATCH --cpus-per-task=7      
+#SBATCH --mem-per-gpu=60G      # CPU RAM per GPU (GPU memory is always 64GB per GPU)
 #SBATCH --time=1:00:00
 
 module use /appl/local/training/modules/AI-20240529
