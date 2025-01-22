368499800f7a3253dff630802284c7e9c1e10ebd
gregordecristoforo <gregor.decristoforo@gmail.com>
2025-01-22 11:42:56 +0200

    add scripts for quickstart chapter

--- visualtransformer.py
+++ visualtransformer.py
@@ -3,19 +3,23 @@ from hdf5_dataset import HDF5Dataset
 import torch
 import torchvision.transforms as transforms
 from torchvision.models import vit_b_16
-# Define transformations
+
+# Define transformations for dataset
 transform = transforms.Compose([
     transforms.Resize(256),
     transforms.CenterCrop(224),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
+
 model = vit_b_16(weights='DEFAULT')
 criterion = torch.nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
 
 def train_model(model, criterion, optimizer, train_loader, val_loader, epochs=10):
+    # note that "cuda" is used as a general reference to GPUs,
+    # even when running on AMD GPUs that use ROCm
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model.to(device)
     for epoch in range(epochs):
