1d6d9b4132241767b821d3f1253a9ecf0e24583e
Gregor Decristoforo <gregor.decristoforo@gmail.com>
2025-01-23 09:32:48 +0100

    Add mlflow to installed packages

--- set_up_environment.sh
+++ set_up_environment.sh
@@ -8,7 +8,7 @@ echo "Choosing the right container for the visualtransformer example"
 export SIF=/appl/local/containers/sif-images/lumi-pytorch-rocm-6.2.1-python-3.12-pytorch-20240918-vllm-4075b35.sif
 
 echo "Extending the container with a virtual environment and install h5py."
-singularity exec $SIF bash -c '$WITH_CONDA && python -m venv visualtransformer-env --system-site-packages && source visualtransformer-env/bin/activate && python -m pip install h5py'
+singularity exec $SIF bash -c '$WITH_CONDA && python -m venv visualtransformer-env --system-site-packages && source visualtransformer-env/bin/activate && python -m pip install h5py mlflow'
 
 # Please have a look at the terms of access (https://www.image-net.org/download.php) before using the dataset
 echo "Copying training data to working directory."
