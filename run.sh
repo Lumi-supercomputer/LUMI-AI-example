b1ba5b9858ca4dec29f7a28492ff4a52bc17e06c
Gregor Decristoforo <gregor.decristoforo@gmail.com>
2025-01-23 07:57:06 +0100

    Add comments to slurm parameters

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --account=project_XXX  # project account to bill 
-#SBATCH --partition=dev-g      # other options are small-g and standard-g
-#SBATCH --gpus-per-node=1      # Number of GPUs per node (max of 8)
-#SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=7      
-#SBATCH --mem-per-gpu=60G      # CPU RAM per GPU (GPU memory is always 64GB per GPU)
-#SBATCH --time=1:00:00
+#SBATCH --account=project_xxxxxxxxx  # project account to bill 
+#SBATCH --partition=dev-g            # other options are small-g and standard-g
+#SBATCH --gpus-per-node=1            # Number of GPUs per node (max of 8)
+#SBATCH --ntasks-per-node=1          # Use one task for one GPU
+#SBATCH --cpus-per-task=7            # Use 1/8 of all available 56 CPUs on LUMI-G nodes
+#SBATCH --mem-per-gpu=60G            # CPU RAM per GPU (GPU memory is always 64GB per GPU)
+#SBATCH --time=1:00:00               # time limit
 
 module use /appl/local/training/modules/AI-20240529
 module load singularity-userfilesystems singularity-CPEbits
