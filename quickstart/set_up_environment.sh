6c2dcc00c71a03f785d7f478075e1d6463d9d714
Gregor Decristoforo <gregor.decristoforo@gmail.com>
2025-01-30 15:08:06 +0100

    fix typo

--- quickstart/set_up_environment.sh
+++ quickstart/set_up_environment.sh
@@ -5,7 +5,7 @@ module use /appl/local/containers/ai-modules
 module load singularity-AI-bindings
 
 # Please have a look at the terms of access (https://www.image-net.org/download.php) before using the dataset
-echo "Copying container, training data and squasfs file to ../resources/ directory."
+echo "Copying container, training data and squashfs file to ../resources/ directory."
 cp /appl/local/training/LUMI-AI-Guide/tiny-imagenet-dataset.hdf5 ../resources/train_images.hdf5
 cp /appl/local/training/LUMI-AI-Guide/visualtransformer-env.sqsh ../resources/
 cp /appl/local/containers/sif-images/lumi-pytorch-rocm-6.2.1-python-3.12-pytorch-20240918-vllm-4075b35.sif ../resources/
